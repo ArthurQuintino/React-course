d737001a52474f20363a5c1b32bfdbbb1d526128
ArthurQuintino <arthurquintino2304@gmail.com>
2025-02-03 16:33:34 -0300

    Alterações de aparencia do codigo

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -7,10 +7,18 @@ function TaskPage() {
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="h-80 space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
-        <h1 className=" w-64 bg-slate-500 text-white rounded-md shadow flex flex-col text-center">Titulo da Tarefa:</h1>
-        <h1 className="w-64 bg-slate-400 text-white rounded-md shadow flex flex-col justify-normal p-1.5">{title}</h1>
-        <h1 className=" w-64 bg-slate-500 text-white rounded-md shadow flex flex-col text-center" >Descrição da Tarefa</h1>
-        <p className="w-64 bg-slate-400 text-white rounded-md shadow flex flex-col justify-normal p-1.5">{description}</p>
+        <h1 className=" w-64 bg-slate-500 text-white rounded-md shadow flex flex-col text-center">
+          Titulo da Tarefa:
+        </h1>
+        <h1 className="w-64 bg-slate-400 text-white rounded-md shadow flex flex-col justify-normal p-1.5">
+          {title}
+        </h1>
+        <h1 className=" w-64 bg-slate-500 text-white rounded-md shadow flex flex-col text-center">
+          Descrição da Tarefa
+        </h1>
+        <p className="w-64 bg-slate-400 text-white rounded-md shadow flex flex-col justify-normal p-1.5">
+          {description}
+        </p>
       </div>
     </div>
   );
